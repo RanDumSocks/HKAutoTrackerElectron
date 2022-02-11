1f9904a07d5c3ffd273b367afd97e6e758712a25
RanDumSocks <justinchance@randumsocks.com>
2022-02-10 16:08:31 -0800

    settings and main window creation

---
+++ helper/settings.js
@@ -0,0 +1,94 @@
+// Settings management, should only be visible to the main process
+const path = require('path')
+const fs = require('fs')
+
+module.exports = {
+   settingsPath: path.resolve(
+      process.env.APPDATA,
+      'HKAutoTracker/settings.json'
+   ),
+   defaultOptions: {
+      translationType: 'full',
+      mapOrientation: 'LR',
+      benchPathfinding: true,
+      lastVersion: undefined,
+   },
+   options: {},
+
+   get translationType() {
+      return this.options.translationType
+   },
+
+   set translationType(value) {
+      if (["full", "basic", "landmark", "none"].includes(value)) {
+         this.options.translationType = value
+      } else {
+         console.log(`Invalid translationType option ${value}`)
+      }
+   },
+
+   get mapOrientation() {
+      return this.options.mapOrientation
+   },
+
+   set mapOrientation(value) {
+      if (["TB", "TD", "BT", "RL", "LR"].includes(value)) {
+         this.options.mapOrientation = value
+      } else {
+         console.log(`Invalid mapOrientation option ${value}`)
+      }
+   },
+
+   get benchPathfinding() {
+      return this.options.benchPathfinding
+   },
+
+   set benchPathfinding(value) {
+      if (typeof(value) == 'boolean') {
+         this.options.benchPathfinding = value
+      } else {
+         console.log(`Invalid benchPathfinding option ${value}`)
+      }
+   },
+
+   get lastVersion() {
+      return this.options.lastVersion
+   },
+
+   set lastVersion(value) {
+      this.options.lastVersion = value
+   },
+
+   loadSettings: () => {
+      var userSettings = undefined
+      try {
+         userSettings = JSON.parse(fs.readFileSync(module.exports.settingsPath))
+      } catch (err) {
+         console.log('Settings file corrupt, resetting to default settings.')
+         module.exports.options = module.exports.defaultOptions
+         return
+      }
+
+      for (const [settingName, value] of Object.entries(userSettings)) {
+         if (Object.getOwnPropertyDescriptor(module.exports, settingName)?.set) {
+            module.exports[settingName] = value
+         } else {
+            console.log(`Unknown setting ${settingName} found in user settings.`)
+         }
+      }
+
+      module.exports.options = { ...module.exports.defaultOptions, ...module.exports.options}
+   },
+
+   // Should only be called on application exit
+   writeSettings: () => {
+      console.log(module.exports.options)
+      fs.writeFileSync(
+         module.exports.settingsPath,
+         JSON.stringify(module.exports.options, null, 3)
+      )
+      console.log("DONE")
+   },
+}
+
+module.exports.loadSettings()
---
+++ main.js
@@ -0,0 +1,219 @@
+const {app, BrowserWindow, Menu} = require('electron')
+const settings = require('./helper/settings')
+const path = require('path')
+const url = require('url')
+
+const version = app.getVersion()
+
+// Whether app has cleaned up and can continue termination
+var cleanedUp = false
+
+// BrowserWindow objects
+var windows = {
+   main: [undefined, {
+      url: url.format({
+         pathname: path.resolve(__dirname, "tracker.html"),
+         protocol: "file",
+         slashes: true,
+      })
+   }],
+   location: [undefined, {}],
+   nearest: [undefined, {}],
+}
+
+var menuTemplate = [
+   {
+      label: 'File',
+      submenu: [
+         {
+            label: 'Exit',
+            role: 'quit',
+         },
+      ],
+   },
+   {
+      label: 'Settings',
+      submenu: [
+         {
+            label: 'Graph orientation',
+            type: 'submenu',
+            submenu: [
+               {
+                  label: 'Top to Bottom',
+                  type: 'radio',
+                  id: 'TB',
+                  click: () => {
+                     settings.mapOrientation = 'TB'
+                  },
+                  checked: settings.mapOrientation == 'TB',
+               },
+               {
+                  label: 'Left to Right',
+                  type: 'radio',
+                  id: 'LR',
+                  click: () => {
+                     settings.mapOrientation = 'LR'
+                  },
+                  checked: settings.mapOrientation == 'LR',
+               },
+               {
+                  label: 'Right to Left',
+                  type: 'radio',
+                  id: 'RL',
+                  click: () => {
+                     settings.mapOrientation = 'RL'
+                  },
+                  checked: settings.mapOrientation == 'RL',
+               },
+               {
+                  label: 'Bottom to Top',
+                  type: 'radio',
+                  id: 'BT',
+                  click: () => {
+                     settings.mapOrientation = 'BT'
+                  },
+                  checked: settings.mapOrientation == 'BT',
+               },
+            ],
+         },
+         {
+            label: 'Translation Type',
+            type: 'submenu',
+            submenu: [
+               {
+                  label: 'Full',
+                  type: 'radio',
+                  id: 'full',
+                  click: () => {
+                     settings.translationType = 'full'
+                  },
+                  checked: settings.translationType == 'full',
+               },
+               {
+                  label: 'Basic',
+                  type: 'radio',
+                  id: 'basic',
+                  click: () => {
+                     settings.translationType = 'basic'
+                  },
+                  checked: settings.translationType == 'basic',
+               },
+               {
+                  label: 'Landmarks',
+                  type: 'radio',
+                  id: 'landmark',
+                  click: () => {
+                     settings.translationType = 'landmark'
+                  },
+                  checked: settings.translationType == 'landmark',
+               },
+               {
+                  label: 'None',
+                  type: 'radio',
+                  id: 'none',
+                  click: () => {
+                     settings.translationType = 'none'
+                  },
+                  checked: settings.translationType == 'none',
+               },
+            ],
+         },
+         {
+            label: 'sep',
+            type: 'separator',
+         },
+         {
+            label: 'Consider benches for pathfinding',
+            type: 'checkbox',
+            id: 'benchPathfinding',
+            click: () => {
+               settings.benchPathfinding = !settings.benchPathfinding
+            },
+            checked: settings.benchPathfinding,
+         },
+      ],
+   },
+   {
+      label: 'View',
+      submenu: [
+         {
+            label: 'Local Tracker',
+            type: 'checkbox',
+            id: 'localTracker',
+            //click: toggleLocalTracker,
+         },
+         {
+            label: 'Nearest Tracker',
+            type: 'checkbox',
+            id: 'nearestTracker',
+            //click: toggleNearestTracker,
+         },
+         {
+            label: 'sep',
+            type: 'separator',
+         },
+         {
+            label: 'Find Current Location',
+            /*click: () => {
+               winMain.webContents.postMessage('main-message', [
+                  'find-location',
+                  {},
+               ])
+            },*/
+         },
+      ],
+   },
+   {
+      label: 'Help',
+      submenu: [
+         {
+            label: 'Submit Feedback',
+            click: () => {
+               open(
+                  'https://github.com/RanDumSocks/HKAutoTrackerElectron/issues/new/choose'
+               )
+            },
+         },
+         {
+            label: 'Changelog',
+            //click: changelog.showLatest,
+         },
+      ],
+   },
+]
+
+function toggleWindow(windowName, url) {
+   let windowInstance = windows[windowName][0]
+   let windowOptions = windows[windowName][1]
+
+   if (!windowInstance) {
+      windowInstance = new BrowserWindow(windowOptions)
+      windowInstance.loadURL(windowOptions.url)
+      windows[windowName][0] = windowInstance
+   } else {
+      windowInstance.close()
+      windows[windowName][0] = undefined
+   }
+
+   return windowInstance
+}
+
+app.whenReady().then(() => {
+   toggleWindow('main')
+   Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate))
+
+   app.on('before-quit', (e) => {
+      if (!cleanedUp) {
+         e.preventDefault()
+         settings.writeSettings()
+         cleanedUp = true
+         app.quit()
+      }
+   })
+
+   app.on('window-all-closed', () => {
+      if (process.platform !== 'darwin') {
+         app.quit()
+      }
+   })
+})
